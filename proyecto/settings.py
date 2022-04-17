0bbcb4fff5be11de60ace08c21bbe99ac5dc6014
Brayan <brayansachez67@hotmail.com>
2022-04-16 22:00:22 -0500

    .env now working

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -12,13 +12,15 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import environ
-import os
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+#env = environ.Env(
+#    # set casting, default value
+#    DEBUG=(bool, False)
+#)
 
+#inicialize environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
