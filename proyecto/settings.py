0bb68b69a3f682d10c86c784aa34f5597d7de5b8
Brayan <brayansachez67@hotmail.com>
2022-04-16 21:41:17 -0500

    fix .env

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 import environ
 import os
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
